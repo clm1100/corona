8290b97f3c0b4f66b21a8106bfcfbbdd50e50cfa
dellclm1100 <275298711@qq.com>
2019-05-23 19:50:54 +0800

    更新

---
+++ js/app2.js
@@ -0,0 +1,40 @@
+class Colorn{
+    constructor(){
+        this.deg = 0
+        this.sTime = Date.now()
+        this.duration = 1000
+        this.p = 0
+        this.speed = 0
+        this.maxSpeed = 15
+        this.stopDeg = null
+        this.canvas = document.querySelector('.dial')
+    }
+    loop() {
+        requestAnimationFrame( ()=> {
+            this.p = (Date.now() - this.sTime) / this.duration
+            if (this.stopDeg) {
+                this.speed = this.maxSpeed * (1 - this.p)
+                if (this.speed <= 0) return this.speed = 0
+            } else {
+                this.speed = this.maxSpeed * this.p
+                if (this.speed > this.maxSpeed) this.speed = this.maxSpeed
+            }
+    
+            this.deg += this.speed
+            this.canvas.style.transform = 'rotate(' + this.deg + 'deg) translate3d(0,0,0)'
+            this.loop()
+        })
+    }
+    stop(){
+        this.stopDeg  = 78
+        this.sTime    = Date.now()
+        this.duration = 3000
+    }
+}
+
+var colorn = new Colorn();
+colorn.loop();
+
+document.querySelector("h1").addEventListener("click",()=>{
+    colorn.stop();
+})
